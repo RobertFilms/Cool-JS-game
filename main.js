b77133cd8c4136d45ab7f597dac7a0cf02f9db3b
RobertFilms <robertamggt3@gmail.com>
2023-11-30 14:57:27 -0500

    did stuff
    I was pondering on this indeed

---
+++ main.js
@@ -0,0 +1,14 @@
+let firstName = "Robert ";// This does stuff
+let lastName = "Ambartsumyan ";
+let age;
+let student = true;
+age = 15;
+/*
+poopy python like code
+*/
+console.log("The user is", firstName, lastName, "and their age is", age);
+console.log("Are they a student?", student) // So basically, this like, makes the computer do stuff or smth like that I think
+
+document.getElementById("p1").innerHTML = "Hello " + firstName;
+document.getElementById("p2").innerHTML = "You are " + age + "years old";
+document.getElementById("p3").innerHTML = "Enrolled: " + student;
