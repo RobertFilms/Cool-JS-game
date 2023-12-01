87d7a20f5edc43295718aed531b5af48af1b8b8f
RobertFilms <robertamggt3@gmail.com>
2023-12-01 11:20:22 -0500

    did stuff

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
-let firstName = "Robert ";// This does stuff
-let lastName = "Ambartsumyan ";
-let age;
-let student = true;
-age = 15;
-/*
-poopy python like code
-*/
-console.log("The user is", firstName, lastName, "and their age is", age);
-console.log("Are they a student?", student) // So basically, this like, makes the computer do stuff or smth like that I think
+//set game set up
+let userIMP;
+let userValid;
 
-document.getElementById("p1").innerHTML = "Hello " + firstName;
-document.getElementById("p2").innerHTML = "You are " + age + "years old";
-document.getElementById("p3").innerHTML = "Enrolled: " + student;
+//Gets first and last name from user
+let firstName = window.prompt("What is your first name?");
+let lastName = window.prompt("What is your last name?");
+let age = window.prompt("What is your age?");
+
+//prints name into conslole
+console.log(firstName);
+console.log(lastName);
+
+//checks if user is old enough
+if (age <= 14) {
+    document.getElementById("funnyLog").innerHTML = "YOU ARE TOO YOUNG!";
+    userValid = false;
+
+} else {
+    document.getElementById("funnyLog").innerHTML = "Welcome " + age + " year old.";
+    userValid = true;
+}
+
+//tells user their info
+if (userValid) {
+    document.getElementById("p1").innerHTML = "Hello " + firstName + " " + lastName + "!";
+
+    //actual game part i guess
+    document.getElementById("button").onclick = function () {
+
+        userIMP = document.getElementById("awnser").value;
+        console.log("worked");
+        //document.getElementById("gameText").innerHTML = userIMP;
+        document.getElementById('printed').innerHTML += "<h1>"+userIMP+"</h1>";
+    }
+
+// hides button if 14 or under
+} else {
+
+    document.getElementById('stuff').hidden = true;
+
+}
