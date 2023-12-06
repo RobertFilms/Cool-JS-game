69128c931e10adb10f5da89ed6a091cb57a22b41
Robert <123018999+rober26522@users.noreply.github.com>
2023-12-06 14:16:28 -0500

    did some stuff ig

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ THINGS TO ADD/FIX
 */
 
 //set game set up
-let userIMP;
+let userInp;
 let userValid;
 
 //Gets first and last name from user
@@ -15,44 +15,39 @@ let firstName = window.prompt("What is your first name?");
 let lastName = window.prompt("What is your last name?");
 let age = window.prompt("What is your age?");
 
-//prints name into conslole
 console.log(firstName);
 console.log(lastName);
 
-//checks if user is old enough
 if (age <= 14) {
     document.getElementById("funnyLog").innerHTML = "YOU ARE TOO YOUNG!";
     userValid = false;
-
-} else if (age > 35) {
+  } else if (age > 35) {
     document.getElementById("funnyLog").innerHTML = "MR SMITH AGE";
-    userValid = false
-
-} else {
+    userValid = false;
+  } else {
     document.getElementById("funnyLog").innerHTML = "Welcome " + age + " year old.";
     userValid = true;
-}
+  }
 
-//tells user their info
 if (userValid) {
     document.getElementById("p1").innerHTML = "Hello " + firstName + " " + lastName + "!";
-
-    //actual game part i guess
     document.getElementById("button").onclick = function () {
-
-        userIMP = document.getElementById("awnser").value;
-        console.log("worked.");
-        //document.getElementById("gameText").innerHTML = userIMP;
-        document.getElementById('printed').innerHTML += "<h1>"+userIMP+"</h1>";
+      userInp = document.getElementById("awnser").value;
+      console.log("worked.");
+      document.getElementById('printed').innerHTML += "<h1>"+userInp+"</h1>";
     }
-    //make a delete button.
     document.getElementById("delete").onclick = function () {
-        console.log("Deleteing awnser...");
-        document.getElementById('printed').innerHTML -= "<h1>"+userIMP+"</h1>";
+      console.log("Deleting answer...");
+      // Select all 'h1' elements inside the 'printed' div
+      let h1Element = document.getElementById('printed').getElementsByTagName('h1');
+      
+      // Check if there are 'h1' elements present
+      if (h1Element.length > 0) {
+        // Remove the last 'h1' element
+        h1Element[h1Element.length - 1].remove();
+      }
     }
-// hides button if 14 or under
-} else {
-
+  } else {
     document.getElementById('stuff').hidden = true;
+  }
 
-}
